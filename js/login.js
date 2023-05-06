f928e77585f1b9cc0cc1441054b9040133c03858
wochae <officialwochae@gmail.com>
2023-05-06 21:11:47 +0900

    [fix] : adding some tin specifically #2

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ login.onclick = function(){
     var password = document.getElementById("pw").value;
     if(username == "id" && password == "pw"){
         var link = "/html/form2.html"
-        window.location.href(link); // 이거 안됨
+        window.location.href(link); // 이거 이제 됨. // form 안에 action에 위 link 주소를 넣어야 값을 넘겨주기 때문에 명시해야함.
         // window.open(link); // 새 창으로 떠서 이동은 됨.
     } else {
         alert("그대로 쓰세요.");
