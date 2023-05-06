ffd2398671d75864eacf1545011ec50d2d333367
wochae <officialwochae@gmail.com>
2023-05-06 16:00:21 +0900

    [feat] : 입력 폼 로그인 성공 시 페이지 반환 #2

---
+++ js/login.js
@@ -0,0 +1,12 @@
+var login = document.getElementById("submit");
+login.onclick = function(){
+    var username = document.getElementById("id").value;
+    var password = document.getElementById("pw").value;
+    if(username == "id" && password == "pw"){
+        var link = "/html/form2.html"
+        // window.location.href(link); // 이거 안됨
+        window.open(link); // 새 창으로 떠서 이동은 됨.
+    } else {
+        alert("그대로 쓰세요.");
+    }
+}
