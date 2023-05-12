767f7911e7ba1c9d176f7d6e752e45424f939d10
wochae <officialwochae@gmail.com>
2023-05-12 16:14:55 +0900

    [feat] : adding img tag easily too #5

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,16 +5,14 @@ import './App.css';
 function App() {
 
   let posts = '강남 고기 맛집';
-  function 함수() {
-    return 100
-  }
   
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
-        <h4>{ posts }</h4>
       </div>
+      <img src={ logo } />
+        <h4>{ posts }</h4>
     </div>
   );
 }
