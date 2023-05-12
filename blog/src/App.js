02570f1eef436d5082b8e96017fe934d33f8390e
wochae <officialwochae@gmail.com>
2023-05-12 14:42:42 +0900

    [feat] : data binding in react better than former way #5

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '강남 고기 맛집';
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
+        <h4>{ posts }</h4>
       </div>
     </div>
   );
