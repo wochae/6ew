a7eeccef8b8e1166a2803ee033bbb0378d809fb9
wochae <officialwochae@gmail.com>
2023-05-13 14:42:03 +0900

    [feat] : thumbs up counting with onClick function, useState 2nd parameter #7

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,13 +6,14 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   let posts = '강남 고기 맛집';
+  let [따봉, 따봉변경] = useState(0);
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1)} }>👍</span> {따봉} </h3>
         <p>5월 13일 발행</p>
         <hr/>
       </div>
