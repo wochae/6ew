e0c81a5f4bccd88c7ac0defbee7361e2c3d92e19
wochae <officialwochae@gmail.com>
2023-05-13 15:23:58 +0900

    [feat] : The Component for more modularity #8

--- blog/src/App.js
+++ blog/src/App.js
@@ -34,8 +34,18 @@ function App() {
         <p>5월 15일 발행</p>
         <hr/>
       </div>
+      <Modal/>
     </div>
   );
+}
+function Modal(){
+  return (
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
