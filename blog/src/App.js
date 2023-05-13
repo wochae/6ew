2b900408dc796054ea323cde8cc9967d2a12ac55
wochae <officialwochae@gmail.com>
2023-05-13 14:03:38 +0900

    [error] : incorrect answer #6

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState('남자 코트 추천');
+  let [글제목1, 글제목변경1] = useState('남자 코트 추천1');
+  let [글제목2, 글제목변경2] = useState('남자 코트 추천2');
+  let [글제목3, 글제목변경3] = useState('남자 코트 추천3');
 
   let posts = '강남 고기 맛집';
   return (
@@ -13,7 +15,13 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목 } </h3>
+        <h3> { 글제목1 } </h3>
+        <p>5월 13일 발행</p>
+        <hr/>
+        <h3> { 글제목2 } </h3>
+        <p>5월 13일 발행</p>
+        <hr/>
+        <h3> { 글제목3 } </h3>
         <p>5월 13일 발행</p>
         <hr/>
       </div>
