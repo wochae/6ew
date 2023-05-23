d1d836e359e79fd0fc778cbda9971e002c88e38d
wochae <officialwochae@gmail.com>
2023-05-23 21:24:05 +0900

    [feat] : TodoItem styling from react-icons've installed #12

--- blog/src/components/TodoItem.js
+++ blog/src/components/TodoItem.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {MdCheckBox, MdCheckBoxOutlineBlank} from 'react-icons/md';
 
 const TodoItem = ({todo}) => {
-    const {text} = todo;
-    return <div>{text}</div>
-} ;
+    const { id, text, checked } = todo;
+    return <div className="TodoItem">
+        <div className={`content ${checked ? 'checked' : ''}`}>
+            {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
+            <div>{text}</div>
+        </div>
+    </div>    
+};
 
 export default TodoItem;
