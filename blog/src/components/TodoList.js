ab752fe51b320cec4e35f5321e60e85290b8a349
wochae <officialwochae@gmail.com>
2023-05-23 21:43:23 +0900

    [feat] : TodoList margin same as Template styles #12

--- blog/src/components/TodoList.js
+++ blog/src/components/TodoList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import './TodoList.css';
 
 const TodoList = ({todos}) => {
     return (
-        <div>
+        <div className="TodoList">
             {todos.map(todo => (
                 <TodoItem todo={todo} key={todo.id}/>
             ))}
