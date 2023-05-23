1a01117f73edf3de532423aca0497bc181610b4e
wochae <officialwochae@gmail.com>
2023-05-23 21:32:55 +0900

    [feat] : title css within Template.js #12

--- blog/src/components/Template.js
+++ blog/src/components/Template.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Template.css';
 
 const Template = ({ children }) => {
     return (
-        <div>
-            <div>오늘의 할 일 (0)</div>
+        <div className="Template">
+            <div className="title">오늘의 할 일 (0)</div>
             <div>{children}</div>
         </div>
     );
