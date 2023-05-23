e985435bcef97081d92a19e1e67a1199794a1477
wochae <officialwochae@gmail.com>
2023-05-23 20:41:17 +0900

    [fix] : TodoList wrong statements fixed, adding State Hook #12

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,17 @@
-import React, { Children } from 'react';
+import React, { useState } from 'react';
 import Template from './components/Template';
 import TodoList from './components/TodoList';
 import './App.css';
 
 const App = () => {
+  const [todos, setTodos] = useState( [
+    { id: 1, text: '리액트의 기초 알아보기', checked: true },
+    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
+    { id: 3, text: '일정 관리 앱 만들어 보기', checked: false }
+  ]); 
   return( 
     <Template>
-      <TodoList />
+      <TodoList todos={todos}/>
     </Template>
   );
 };
--- blog/src/components/TodoList.js
+++ blog/src/components/TodoList.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const TodoList = ({todos}) => {
-    return <div>{todos.map(<div>{todos.text}</div>)}</div>
+    return (
+        <div>
+            {todos.map(todos => (
+                <div>{todos.text}</div>
+            ))}
+        </div>
+    );
 };
 
-export default TodoList;
+export default TodoList; 
