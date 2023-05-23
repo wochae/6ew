c606dc7520e31a1ddb7fd8ca8a7a8ca035bfbbbe
wochae <officialwochae@gmail.com>
2023-05-23 21:00:18 +0900

    [feat] : key prop for Each child in a list should have a unique #12

--- blog/src/components/TodoList.js
+++ blog/src/components/TodoList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import TodoItem from './TodoItem';
 
 const TodoList = ({todos}) => {
     return (
         <div>
-            {todos.map(todos => (
-                <div>{todos.text}</div>
+            {todos.map(todo => (
+                <TodoItem todo={todo} key={todo.id}/>
             ))}
         </div>
     );
